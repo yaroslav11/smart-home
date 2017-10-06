0c7d00a4198e3b3e0b890cc088204dc879734efd
yaroslav11 <yaroslav.zhukov@phystech.edu>
2017-10-06 18:04:45 +0300

    Refactor methods for reading home information into new class

--- src/main/java/ru/sbt/mipt/oop/Application.java
+++ src/main/java/ru/sbt/mipt/oop/Application.java
@@ -1,10 +1,6 @@
 package ru.sbt.mipt.oop;
 
-import com.google.gson.Gson;
-
 import java.io.IOException;
-import java.nio.file.Files;
-import java.nio.file.Paths;
 
 import static ru.sbt.mipt.oop.SensorEventType.*;
 
@@ -12,9 +8,7 @@ public class Application {
 
     public static void main(String... args) throws IOException {
         // считываем состояние дома из файла
-        Gson gson = new Gson();
-        String json = new String(Files.readAllBytes(Paths.get("smart-home-1.js")));
-        SmartHome smartHome = gson.fromJson(json, SmartHome.class);
+        SmartHome smartHome = HomeJsonFileReader.read();
         // начинаем цикл обработки событий
         SensorEvent event = getNextSensorEvent();
         while (event != null) {
---
+++ src/main/java/ru/sbt/mipt/oop/HomeJsonFileReader.java
@@ -0,0 +1,15 @@
+package ru.sbt.mipt.oop;
+
+import com.google.gson.Gson;
+
+import java.io.IOException;
+import java.nio.file.Files;
+import java.nio.file.Paths;
+
+public class HomeJsonFileReader {
+    public static SmartHome read() throws IOException {
+        Gson gson = new Gson();
+        String json = new String(Files.readAllBytes(Paths.get("smart-home-1.js")));
+        return gson.fromJson(json, SmartHome.class);
+    }
+}
